14d1c9992abffa6c57cd04b3a7765b99159f47bc
abdulmuiz22 <osuolaleabdulmuiz22@gmail.com>
2026-08-11 19:42:00 +0100

    fix message section

--- app/Models/User.php
+++ app/Models/User.php
@@ -143,4 +143,14 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(CommunicationLog::class, 'sender_id');
     }
+
+    public function sentMessages()
+    {
+        return $this->hasMany(Message::class, 'sender_id');
+    }
+
+    public function receivedMessages()
+    {
+        return $this->hasMany(Message::class, 'receiver_id');
+    }
 }
